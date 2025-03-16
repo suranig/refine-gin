86e6bee3a9462cce6beaf720a3a561140b114505
tumiwisista <pawpilichowski@gmail.com>
2025-03-16 23:06:09 +0100

    feat(repository): Implement GormRepository with CRUD operations

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -5,28 +5,85 @@ import (
 
 	"github.com/suranig/refine-gin/pkg/query"
 	"github.com/suranig/refine-gin/pkg/resource"
+	"gorm.io/gorm"
 )
 
-// Repository reprezentuje repozytorium dla zasobu
 type Repository interface {
-	// List pobiera listę elementów z opcjami zapytania
 	List(ctx context.Context, options query.QueryOptions) (interface{}, int64, error)
 
-	// Get pobiera element po ID
 	Get(ctx context.Context, id interface{}) (interface{}, error)
 
-	// Create tworzy nowy element
 	Create(ctx context.Context, data interface{}) (interface{}, error)
 
-	// Update aktualizuje istniejący element
 	Update(ctx context.Context, id interface{}, data interface{}) (interface{}, error)
 
-	// Delete usuwa element
 	Delete(ctx context.Context, id interface{}) error
 }
 
-// RepositoryFactory tworzy repozytorium dla zasobu
 type RepositoryFactory interface {
-	// CreateRepository tworzy repozytorium dla zasobu
 	CreateRepository(res resource.Resource) Repository
+}
+
+type GormRepository struct {
+	DB    *gorm.DB
+	Model interface{}
+}
+
+func (r *GormRepository) List(ctx context.Context, options query.QueryOptions) (interface{}, int64, error) {
+	slicePtr := resource.CreateSliceOfType(r.Model)
+
+	total, err := options.ApplyWithPagination(r.DB.Model(r.Model), slicePtr)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return slicePtr, total, nil
+}
+
+func (r *GormRepository) Get(ctx context.Context, id interface{}) (interface{}, error) {
+	item := resource.CreateInstanceOfType(r.Model)
+
+	if err := r.DB.First(item, "id = ?", id).Error; err != nil {
+		return nil, err
+	}
+
+	return item, nil
+}
+
+func (r *GormRepository) Create(ctx context.Context, data interface{}) (interface{}, error) {
+	if err := r.DB.Create(data).Error; err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func (r *GormRepository) Update(ctx context.Context, id interface{}, data interface{}) (interface{}, error) {
+	// Ustaw ID w danych, jeśli to możliwe
+	if err := resource.SetID(data, id); err != nil {
+		return nil, err
+	}
+
+	if err := r.DB.Save(data).Error; err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func (r *GormRepository) Delete(ctx context.Context, id interface{}) error {
+	item := resource.CreateInstanceOfType(r.Model)
+
+	if err := resource.SetID(item, id); err != nil {
+		return err
+	}
+
+	return r.DB.Delete(item).Error
+}
+
+func NewGormRepository(db *gorm.DB, model interface{}) Repository {
+	return &GormRepository{
+		DB:    db,
+		Model: model,
+	}
 }
